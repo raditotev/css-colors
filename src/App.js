c4c60e4532bf134796dcf318f273cc76ce71aa54
Radi Totev <raditotev@gmail.com>
2022-05-23 22:04:24 +0300

    Complete apps functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { useState } from 'react';
+
+import { COLORS, darkColors } from './assets/colors';
+import CopyText from './components/CopyText';
+
 function App() {
-  return <div className="App">hello</div>;
+  const [copied, setCopied] = useState(false);
+
+  const copyHandler = (color) => {
+    navigator.clipboard.writeText(color);
+    setCopied(true);
+  };
+
+  return COLORS.map((color) => {
+    const style = {
+      backgroundColor: color,
+      color: darkColors.includes(color) ? 'WhiteSmoke' : 'Black',
+    };
+    return (
+      <div
+        key={color}
+        className="colorbox"
+        style={style}
+        onClick={() => copyHandler(color)}
+        onMouseLeave={() => setCopied(false)}
+      >
+        <CopyText color={style.color} copied={copied} />
+        <h3>{color}</h3>
+      </div>
+    );
+  });
 }
 
 export default App;
