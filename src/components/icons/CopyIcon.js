7cb70777a0a50d6cc1132f1f0246ff6d24b81fa1
Radi Totev <raditotev@gmail.com>
2022-05-24 10:13:46 +0300

    Fix jsx attribute error

--- src/components/icons/CopyIcon.js
+++ src/components/icons/CopyIcon.js
@@ -7,7 +7,7 @@ const CopyIcon = () => {
       x="0px"
       y="0px"
       viewBox="0 0 64 64"
-      enable-background="new 0 0 64 64"
+      enableBackground="new 0 0 64 64"
     >
       <g id="Text-files">
         <path
