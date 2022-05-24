f783e2a35a705d0910c1887624c66354e35c79ca
Radi Totev <raditotev@gmail.com>
2022-05-24 10:10:47 +0300

    Create ColorBox component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
-import { useState } from 'react';
-
-import { COLORS, darkColors } from './assets/colors';
-import CopyText from './components/CopyText';
+import { COLORS } from './assets/colors';
+import ColorBox from './components/ColorBox';
 
 function App() {
-  const [copied, setCopied] = useState(false);
-
-  const copyHandler = (color) => {
-    navigator.clipboard.writeText(color);
-    setCopied(true);
-  };
-
-  return COLORS.map((color) => {
-    const style = {
-      backgroundColor: color,
-      color: darkColors.includes(color) ? 'WhiteSmoke' : 'Black',
-    };
-    return (
-      <div
-        key={color}
-        className="colorbox"
-        style={style}
-        onClick={() => copyHandler(color)}
-        onMouseLeave={() => setCopied(false)}
-      >
-        <CopyText color={style.color} copied={copied} />
-        <h3>{color}</h3>
-      </div>
-    );
-  });
+  return COLORS.map((color) => <ColorBox color={color} />);
 }
 
 export default App;
---
+++ src/components/ColorBox.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+import { darkColors } from '../assets/colors';
+import CopyText from './CopyText';
+
+const ColorBox = ({ color }) => {
+  const [copied, setCopied] = useState(false);
+
+  const style = {
+    backgroundColor: color,
+    color: darkColors.includes(color) ? 'WhiteSmoke' : 'Black',
+  };
+
+  const copyHandler = (color) => {
+    navigator.clipboard.writeText(color.toLowerCase());
+    setCopied(true);
+  };
+
+  return (
+    <div
+      key={color}
+      className="color-box"
+      style={style}
+      onClick={() => copyHandler(color)}
+      onMouseLeave={() => setCopied(false)}
+    >
+      <CopyText color={style.color} copied={copied} />
+      <h3>{color}</h3>
+    </div>
+  );
+};
+
+export default ColorBox;
