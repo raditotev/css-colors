7db9464778823938827bbc18e1d4b8822142356f
Radi Totev <raditotev@gmail.com>
2022-05-25 12:52:17 +0300

    Show colors by group

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import { COLORS } from './assets/colors';
+import { useState } from 'react';
+
+import { getColors, getColorGroups } from './lib/helpers';
 import ColorBox from './components/ColorBox';
+import ColorGroupItem from './components/ColorGroupItem';
 
 function App() {
-  return COLORS.map((color) => <ColorBox key={color} color={color} />);
+  const [selectedColor, setSelectedColor] = useState('all');
+  const colors = getColors(selectedColor);
+
+  const selectHandler = (group) => {
+    setSelectedColor((state) => (state === group ? 'all' : group));
+  };
+
+  return (
+    <>
+      <div className="color-groups">
+        <h3>Color categories</h3>
+        {getColorGroups.map((group) => (
+          <ColorGroupItem
+            key={group}
+            group={group}
+            active={group === selectedColor}
+            selectHandler={selectHandler}
+          />
+        ))}
+      </div>
+      <div className="list-colors">
+        {colors.map((color) => (
+          <ColorBox key={color} color={color} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
